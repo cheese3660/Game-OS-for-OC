a555ee992fd5350a9784f272e42c07a066ed090f
cheese3660 <arallen3660@gmail.com>
2018-01-17 20:26:41 -0500

    Update map.lua

--- lib/map.lua
+++ lib/map.lua
@@ -1,7 +1,25 @@
 local map = {}
 map.layers = {}
-function map:load(address,file)
+function map:load(file,address)
   local fs = component.proxy(address)
-
+  local handle = fs.open(file,"rb")
+  local layers = read(handle,1)
+  self.w = read(handle,1)
+  self.h = read(handle,1)
+  for i = 1,layers do
+    self.layers[i] = {}
+    for y = 1, self.w do
+      self.layers[i][y] = {}
+      for x = 1, self.h do
+        self.layers[i][y][x] = read(handle,1)
+      end
+    end
+  end
+end
+function map:get(x,y,l)
+  return self.layers[l][y][x]
+end
+function map:set(x,y,l,tile)
+  self.layers[l][y][x] = tile
 end
 return map
